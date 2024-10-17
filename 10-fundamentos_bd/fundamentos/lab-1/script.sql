9a4d90a3fdf39b4de9abe76a83d7fe10c67735f8
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-10-17 18:25:17 -0400

    feat: Atualizando arquivo sql

--- 10-fundamentos_bd/fundamentos/lab-1/script.sql
+++ 10-fundamentos_bd/fundamentos/lab-1/script.sql
@@ -1,65 +1,247 @@
-CREATE TABLE clientes (
-    id SERIAL PRIMARY KEY,
-    nome_completo VARCHAR(255) NOT NULL,
-    cpf VARCHAR(14) UNIQUE NOT NULL,
-    numero_celular VARCHAR(20),
-    email VARCHAR(255) UNIQUE NOT NULL,
-    data_nascimento DATE,
-    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: lojaWeb
+-- ------------------------------------------------------
+-- Server version	8.0.39
 
-CREATE TABLE enderecos (
-    id SERIAL PRIMARY KEY,
-    cliente_id INT REFERENCES clientes(id) ON DELETE CASCADE,
-    logradouro VARCHAR(255) NOT NULL,
-    numero VARCHAR(10) NOT NULL,
-    complemento VARCHAR(255),
-    bairro VARCHAR(255) NOT NULL,
-    cidade VARCHAR(255) NOT NULL,
-    estado VARCHAR(2) NOT NULL,
-    cep VARCHAR(10) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE categorias (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-);
+--
+-- Table structure for table `categorias`
+--
 
-CREATE TABLE subcategorias (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    categoria_id INT REFERENCES categorias(id) ON DELETE CASCADE
-);
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE produtos (
-    id SERIAL PRIMARY KEY,
-    modelo VARCHAR(255) NOT NULL,
-    fabricante VARCHAR(255),
-    preco_base NUMERIC(10, 2) NOT NULL,
-    quantidade_disponivel INT NOT NULL,
-    subcategoria_id INT REFERENCES subcategorias(id) ON DELETE SET NULL
-);
+--
+-- Dumping data for table `categorias`
+--
 
-CREATE TABLE numeros_serie (
-    id SERIAL PRIMARY KEY,
-    numero_serie VARCHAR(255) NOT NULL,
-    produto_id INT REFERENCES produtos(id) ON DELETE CASCADE
-);
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE compras (
-    id SERIAL PRIMARY KEY,
-    cliente_id INT REFERENCES clientes(id) ON DELETE CASCADE,
-    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    desconto_aplicado NUMERIC(5, 2),
-    forma_pagamento VARCHAR(50) NOT NULL,
-    total NUMERIC(10, 2) NOT NULL,
-    endereco_envio_id INT REFERENCES enderecos(id) ON DELETE SET NULL
-);
+--
+-- Table structure for table `clientes`
+--
 
-CREATE TABLE itens_compra (
-    id SERIAL PRIMARY KEY,
-    compra_id INT REFERENCES compras(id) ON DELETE CASCADE,
-    produto_id INT REFERENCES produtos(id),
-    quantidade INT NOT NULL,
-    preco NUMERIC(10, 2) NOT NULL
-);
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nome_completo` varchar(255) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `numero_celular` varchar(20) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `data_nascimento` date DEFAULT NULL,
+  `criado_em` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `cpf` (`cpf`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compras`
+--
+
+DROP TABLE IF EXISTS `compras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compras` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `cliente_id` int DEFAULT NULL,
+  `data_hora` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `desconto_aplicado` decimal(5,2) DEFAULT NULL,
+  `forma_pagamento` varchar(50) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `endereco_envio_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compras`
+--
+
+LOCK TABLES `compras` WRITE;
+/*!40000 ALTER TABLE `compras` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enderecos`
+--
+
+DROP TABLE IF EXISTS `enderecos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `enderecos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `cliente_id` int DEFAULT NULL,
+  `logradouro` varchar(255) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `bairro` varchar(255) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cep` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enderecos`
+--
+
+LOCK TABLES `enderecos` WRITE;
+/*!40000 ALTER TABLE `enderecos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `enderecos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `itens_compra`
+--
+
+DROP TABLE IF EXISTS `itens_compra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `itens_compra` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `compra_id` int DEFAULT NULL,
+  `produto_id` int DEFAULT NULL,
+  `quantidade` int NOT NULL,
+  `preco` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `itens_compra`
+--
+
+LOCK TABLES `itens_compra` WRITE;
+/*!40000 ALTER TABLE `itens_compra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `itens_compra` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `numeros_serie`
+--
+
+DROP TABLE IF EXISTS `numeros_serie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `numeros_serie` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `numero_serie` varchar(255) NOT NULL,
+  `produto_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `numeros_serie`
+--
+
+LOCK TABLES `numeros_serie` WRITE;
+/*!40000 ALTER TABLE `numeros_serie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `numeros_serie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `modelo` varchar(255) NOT NULL,
+  `fabricante` varchar(255) DEFAULT NULL,
+  `preco_base` decimal(10,2) NOT NULL,
+  `quantidade_disponivel` int NOT NULL,
+  `subcategoria_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subcategorias`
+--
+
+DROP TABLE IF EXISTS `subcategorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subcategorias` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `categoria_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subcategorias`
+--
+
+LOCK TABLES `subcategorias` WRITE;
+/*!40000 ALTER TABLE `subcategorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subcategorias` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-17 18:22:31
