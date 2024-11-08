26efaefb46853e01ec163b302661abf6d0927b10
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-11-07 20:28:37 -0400

    feat: Vamos Praticar - BootstrapClient

--- 12-react-next/loja-do-filip/src/app/layout.tsx
+++ 12-react-next/loja-do-filip/src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
-// import localFont from "next/font/local";
-// import "./globals.css";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+import BootstrapClient from "./components/BootstrapClient/BootstrapClient";
 
 export const metadata: Metadata = {
   title: "Loja do Fílip",
@@ -27,8 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar title="Loja do Fílip"></Navbar>
+        <Navbar titulo="Loja do Fílip"></Navbar>
         {children}
+        <BootstrapClient></BootstrapClient>
       </body>
     </html>
   );
