7227d95d06a961e3aa2824ed7a7fd0408c95753d
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-11-15 18:05:26 -0400

    feat: React Forms - Prática

--- 12-react-next/loja-do-filip/src/app/cadastro/page.tsx
+++ 12-react-next/loja-do-filip/src/app/cadastro/page.tsx
@@ -1,6 +1,28 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+
+interface CadastroFormData {
+  email: string;
+  confirmarEmail: string;
+  senha: string;
+}
 
 export default function Cadastro() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CadastroFormData>();
+
+  const onSubmit = (data: CadastroFormData) => {
+    console.log(data);
+    router.push('/login');
+  };
+
+
   return (
     <main>
       <div className="container-fluid d-flex min-vh-100">
@@ -9,69 +31,62 @@ export default function Cadastro() {
             <h2>Bem vindo à WA Loja!</h2>
           </div>
           <div className="col-12 col-md-8 d-flex justify-content-center align-items-center">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="nome" className="form-label">
-                  Nome
-                </label>
-                <input
-                  type="text"
-                  className="form-control form-control-lg"
-                  id="nome"
-                  aria-describedby="nome"
-                  required
-                />
-              </div>
+            <div className="form-group">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-3">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className="form-control"
+                    id="email"
+                    type="email"
+                    {...register("email", { required: "O email é obrigatório" })}
+                  />
+                  {errors.email && <span>{errors.email.message}</span>}
+                </div>
 
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  id="email"
-                  aria-describedby="email"
-                  required
-                />
-              </div>
+                <div className="mb-3">
+                  <label htmlFor="confirmarEmail" className="form-label">
+                    Confirmar email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="confirmarEmail"
+                    aria-describedby="confirmarEmail"
+                    {...register("confirmarEmail", { required: "O campo de confirmar email é obrigatório" })}
+                  />
+                  {errors.confirmarEmail && <span>{errors.confirmarEmail.message}</span>}
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="senha">Senha</label>
+                  <input
+                    className="form-control"
+                    id="senha"
+                    type="password"
+                    {...register("senha", {
+                      required: "A senha é obrigatória",
+                      minLength: {
+                        value: 6,
+                        message: "A senha deve ter pelo menos 6 caracteres",
+                      },
+                    })}
+                  />
+                  {errors.senha && <span>{errors.senha.message}</span>}
+                </div>
 
-              <div className="mb-3">
-                <label htmlFor="confirmarEmail" className="form-label">
-                  Confirmar email
-                </label>
-                <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  id="confirmarEmail"
-                  aria-describedby="confirmarEmail"
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="senha" className="form-label">
-                  Senha
-                </label>
-                <input
-                  type="password"
-                  className="form-control form-control-lg"
-                  id="senha"
-                  required
-                />
-              </div>
+                <div className="d-grid col-12">
+                  <button type="submit" className="btn btn-success">
+                    Confirmar cadastro
+                  </button>
+                </div>
 
-              <div className="d-grid col-12">
-                <button type="submit" className="btn btn-success">
-                  Confirmar cadastro
-                </button>
-              </div>
-
-              <div className="text-center mt-3">
-                <Link href="/login" className="btn btn-link">
-                  já possuo cadastro
-                </Link>
-              </div>
-            </form>
+                <div className="text-center mt-3">
+                  <Link href="/login" className="btn btn-link">
+                    já possuo cadastro
+                  </Link>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- 12-react-next/loja-do-filip/src/app/login/page.tsx
+++ 12-react-next/loja-do-filip/src/app/login/page.tsx
@@ -1,53 +1,79 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 import Link from "next/link";
 
+interface LoginFormData {
+  email: string;
+  senha: string;
+}
+
 export default function Login() {
+
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>();
+
+  const onSubmit = (data: LoginFormData) => {
+    console.log(data);
+    router.push("/");
+  };
+
   return (
     <main>
       <div className="container-fluid d-flex min-vh-100">
         <div className="row min-vw-100">
           <div className="col-12 col-md-4 bg-light d-flex justify-content-center align-items-center">
             <h2>Bem vindo à WA Loja!</h2>
-          </div>{" "}
+          </div>
           <div className="col-12 col-md-8 d-flex justify-content-center align-items-center">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="form-control form-control-lg"
-                  id="email"
-                  aria-describedby="email"
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="senha" className="form-label">
-                  Senha
-                </label>
-                <input
-                  type="password"
-                  className="form-control form-control-lg"
-                  id="senha"
-                  required
-                />
-              </div>
+            <div className="form-group">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mb-3">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className="form-control"
+                    id="email"
+                    type="email"
+                    {...register("email", { required: "O email é obrigatório" })}
+                  />
+                  {errors.email && <span>{errors.email.message}</span>}
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="senha">Senha</label>
+                  <input
+                    className="form-control"
+                    id="senha"
+                    type="password"
+                    {...register("senha", {
+                      required: "A senha é obrigatória",
+                      minLength: {
+                        value: 6,
+                        message: "A senha deve ter pelo menos 6 caracteres",
+                      },
+                    })}
+                  />
+                  {errors.senha && <span>{errors.senha.message}</span>}
+                </div>
 
-              <div className="d-grid col-12">
-                <button type="submit" className="btn btn-success">
-                  Entrar
-                </button>
-              </div>
+                <div className="d-grid col-12">
+                  <button type="submit" className="btn btn-success">
+                    Entrar
+                  </button>
+                </div>
 
-              <div className="text-center mt-3">
-                <Link href="/cadastro" className="btn btn-link">
-                  não tenho cadastro
-                </Link>
-              </div>
-            </form>
+                <div className="text-center mt-3">
+                  <Link href="/cadastro" className="btn btn-link">
+                    não tenho cadastro
+                  </Link>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
